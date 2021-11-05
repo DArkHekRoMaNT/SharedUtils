96685decd5cb9c54f716d9e15a90aad8d1200a59
DArkHekRoMaNT <darkhekromant@yandex.ru>
2021-11-05 20:45:58 +0300

    Update shuffle

--- src/Extensions/StringExtensions.cs
+++ src/Extensions/StringExtensions.cs
@@ -4,22 +4,21 @@ namespace SharedUtils.Extensions
 {
     public static class StringExtensions
     {
-        public static string Shuffle(this string str)
+        public static string Shuffle(this string str, Random rnd)
         {
             char[] array = str.ToCharArray();
-            Random rng = new Random();
             int n = array.Length;
             while (n > 1)
             {
                 n--;
-                int k = rng.Next(n + 1);
+                int k = rnd.Next(n + 1);
 
                 if (array[k] == ' ' || array[n] == ' ') continue;
 
                 var value = array[k];
 
-                array[k] = Char.IsUpper(array[k]) ? Char.ToUpper(array[n]) : Char.ToLower(array[n]);
-                array[n] = Char.IsUpper(array[n]) ? Char.ToUpper(array[k]) : Char.ToLower(array[k]);
+                array[k] = char.IsUpper(array[k]) ? char.ToUpper(array[n]) : char.ToLower(array[n]);
+                array[n] = char.IsUpper(array[n]) ? char.ToUpper(value) : char.ToLower(value);
             }
             return new string(array);
         }
